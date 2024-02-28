18243f131a3d6f0edaea50266df9b52a222ffd73
cyrilmuchemi <cyril.muchemi@gmail.com>
2024-02-28 19:32:21 +0300

    Create mobile menu

--- src/index.js
+++ src/index.js
@@ -650,3 +650,16 @@ window.addEventListener('scroll', () => {
   const header = document.querySelector('.hero-navigation');
   header.classList.toggle('sticky', window.scrollY > 0);
 });
+
+const hambaga = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.navigation-list');
+
+hambaga.addEventListener('click', () => {
+  hambaga.classList.toggle('active');
+  navMenu.classList.toggle('active');
+});
+
+document.querySelectorAll('.navlist').forEach((n) => n.addEventListener('click', () => {
+  hambaga.classList.remove('active');
+  navMenu.classList.remove('active');
+}));
